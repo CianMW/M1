df01cc887c5ff177bcba264a0e4874b27fd409af
CianMW <markwick.cian@gmail.com>
2021-08-16 13:56:23 +0100

    Question 11 finished

--- Day6/Day6.js
+++ Day6/Day6.js
@@ -233,17 +233,52 @@ console.log(howManyDays(new Date(2021, 3, 23)))
 /* Ex.10
    Write a function called "isTodayMyBirthday" which should return true if today's your birthday, false otherwise.
 */
+exercise("Exercise 10")
+const isTodayMyBirthday = (birthday) => {
+    let birthMonth = birthday.getMonth()
+    let birthDate = birthday.getDate()
+    let upToDate = new Date()
+    let today = upToDate.getDate()
+    let thisMonth = upToDate.getMonth()
+    let celebration = true
+     if (birthMonth === thisMonth && birthDate === today) {
+        celebration = true
+    } else {
+        celebration = false
+    }
+    return "Today is my birthday :" + celebration
 
+}
+
+console.log(isTodayMyBirthday(new Date(1997, 7, 16)))
 // JS Arrays // Objs
 // NOTE: movies array is defined at the end of this file!
 
 /* Ex.11
    Write a function called "deleteProp" which receives an object and a string as parameters, and returns the given object after deleting its property named as the given string.
 */
+const deleteProp = (obj, string) => {
+    let object = {}
+    Object.assign(object,obj)
+    let prop = string
+    delete object[`${string}`]
+    return object
+}
+
+ let testObject = {
+     name: "kevin",
+     brand: "nike",
+     color: "blue"
+ }
+console.log(deleteProp(testObject,"name"))
 
 /* Ex.12 
     Write a function called "olderMovie" which finds the oldest movie in the array provided at the end of this file.
 */
+const olderMovie = () => {
+
+}
+
 
 /* Ex.13
     Write a function called "countMovies" which returns the number of movies contained in the array provided at the end of this file.
